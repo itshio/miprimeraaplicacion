da547dcf074330c76e16aa0ab597191969bb8d2b
darkclif <morpheoosmatrix@gmail.com>
2018-01-22 10:49:09 +0100

    Initial commit

---
+++ app/src/main/java/com/example/miprimeraaplicacion/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.miprimeraaplicacion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }//fin de on create//
+}//fin de mainactvity//
